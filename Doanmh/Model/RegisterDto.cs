44a1164c57c00e1aeba2750031450af71a790773
meo <bambo18t@gmail.com>
2025-06-12 00:10:10 +0700

    co ban ve data crud va ui login

---
+++ Doanmh/Model/RegisterDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Doanmh.Model
+{
+    public class RegisterDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
